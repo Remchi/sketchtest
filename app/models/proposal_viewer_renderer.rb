5daf449d38ad67d1db01689759352906f337d6ed
Rem Zolotykh <remchi@gmail.com>
2014-05-29 02:58:17 +0300

    Refactor ProposalViewerRenderer: split into small methods
    This refactoring I'm not sure about on this stage of project. I personally would keep version in master branch. In this version though render method is just a command method which lists all operation that must be done.

--- app/models/proposal_viewer_renderer.rb
+++ app/models/proposal_viewer_renderer.rb
@@ -1,33 +1,50 @@
 require 'nokogiri'
 
 class ProposalViewerRenderer
-  def self.render(proposal, html = nil)
-    html = html || File.read(File.join(Rails.root, 'public', 'proposal-template', 'index.html'))
+  def self.render(proposal)
+    html = File.read(File.join(Rails.root, 'public', 'proposal-template', 'index.html'))
 
-    html.gsub!('{client_name}', proposal.client.name)
-    html.gsub!('{proposal_name}', proposal.name)
-    html.gsub!('{proposal_send_date}', proposal.send_date.to_s)
-    html.gsub!('{proposal_user_name}', proposal.user_name)
-    html.gsub!('{client_company}', proposal.client.company)
-    html.gsub!('{client_website}', proposal.client.website)
+    ProposalViewerRenderer.new(proposal, html).render
+  end
 
-    html.gsub!('style/', '/proposal-template/style/')
-    html.gsub!('images/', '/proposal-template/images/')
+  def initialize(proposal, html)
+    @proposal = proposal
+    @html = html
+  end
 
-    html = Nokogiri::HTML(html)
+  def render
+    fix_assets
+    replace_placeholders
+    populate_proposal_section
+    @html
+  end
 
-    proposal_content = html.at_css('#proposal-content')
-    proposal_section = html.at_css('#proposal_section').to_html
+  def fix_assets
+    @html.gsub!('style/', '/proposal-template/style/')
+    @html.gsub!('images/', '/proposal-template/images/')
+  end
+
+  def replace_placeholders
+    @html.gsub!('{client_name}', @proposal.client.name)
+    @html.gsub!('{proposal_name}', @proposal.name)
+    @html.gsub!('{proposal_send_date}', @proposal.send_date.to_s)
+    @html.gsub!('{proposal_user_name}', @proposal.user_name)
+    @html.gsub!('{client_company}', @proposal.client.company)
+    @html.gsub!('{client_website}', @proposal.client.website)
+  end
+
+  def populate_proposal_section
+    @html = Nokogiri::HTML(@html)
+
+    proposal_content = @html.at_css('#proposal-content')
+    proposal_section = @html.at_css('#proposal_section').to_html
     proposal_content.content = ""
 
-    proposal.proposal_sections.each do |section|
+    @proposal.proposal_sections.each do |section|
       node = proposal_section.dup
       node.gsub!('{section_header}', section.name)
       node.gsub!('{section_content}', section.description)
       proposal_content.add_child(node)
     end
-
-
-    html
   end
 end
